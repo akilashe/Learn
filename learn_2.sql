2d9f178957f44cd56cfd6fdec851c4d342874ef0
akilashe <akilasherin24@gmail.com>
2025-05-28 16:46:59 +0530

    learn_1.sql

---
+++ learn_2.sql
@@ -0,0 +1,7 @@
+select * from raid_r_cbs_bc_subscriber;
+select * from raid_r_cbs_bc_customer;
+select * from raid_r_cbs_bc_account;
+select * from raid_r_cbs_bc_sub_iden;
+select * from raid_r_cbs_bc_offer_inst;
+select * from raid_r_cbs_cm_pm_off_base_info;
+select * from raid_r_cbs_bc_offering_prop;
